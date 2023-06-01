d77b859ac309e361f9d831782f00557b4f5d8411
ITSDENISSKA <fotiev.d1991@gmail.com>
2023-06-01 12:54:32 +0300

    drop commit

--- app/src/main/java/com/example/heal/FoodFragment.java
+++ app/src/main/java/com/example/heal/FoodFragment.java
@@ -14,8 +14,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.heal.R;
-
 import java.util.ArrayList;
 import java.util.List;
 
