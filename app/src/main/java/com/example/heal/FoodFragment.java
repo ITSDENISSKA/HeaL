068ef9258979512c767ae39b90c405a20754f221
ShinyTrunk <maksim19032007@gmail.com>
2023-06-03 01:09:04 +0300

    did some design again

--- app/src/main/java/com/example/heal/FoodFragment.java
+++ app/src/main/java/com/example/heal/FoodFragment.java
@@ -36,52 +36,34 @@ public class FoodFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_food, container, false);
-        // Инициализация и настройка ListView и адаптера
         ListView listViewFood = view.findViewById(R.id.listViewFood);
         foodListAdapter = new FoodListAdapter(requireContext(), foodList);
         listViewFood.setAdapter(foodListAdapter);
 
-        // Добавление обработчика нажатия на элемент списка
+
         listViewFood.setOnItemClickListener((adapterView, view1, position, id) -> {
-            // Удаление выбранной еды из списка
             foodList.remove(position);
-            // Обновление адаптера
             foodListAdapter.notifyDataSetChanged();
-            // Сохранение списка еды в SharedPreferences
             saveFoodListToSharedPreferences();
         });
 
-        // Получение ссылок на EditText и кнопку добавления
         EditText editTextFoodName = view.findViewById(R.id.editTextFoodName);
         EditText editTextCalories = view.findViewById(R.id.editTextCalories);
         Button addButton = view.findViewById(R.id.buttonAdd);
 
-        // Добавление обработчика нажатия на кнопку добавления
         addButton.setOnClickListener(v -> {
-            // Получение введенных данных
             String foodName = editTextFoodName.getText().toString().trim();
             String calories = editTextCalories.getText().toString().trim();
-
-            // Проверка на пустые поля
             if (TextUtils.isEmpty(foodName) || TextUtils.isEmpty(calories)) {
                 Toast.makeText(requireContext(), "Введите продукт и калорийность", Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            // Создание нового объекта FoodItem
             FoodItem foodItem = new FoodItem(foodName, calories);
-
-            // Добавление еды в список
             foodList.add(foodItem);
-
-            // Очистка полей ввода
             editTextFoodName.setText("");
             editTextCalories.setText("");
-
-            // Обновление адаптера
             foodListAdapter.notifyDataSetChanged();
-
-            // Сохранение списка еды в SharedPreferences
             saveFoodListToSharedPreferences();
         });
 
